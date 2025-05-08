9db4950f9cbefd97d6bda057e87d0371d371aafb
tuandv25 <doquoctuanit@gmail.com>
2025-05-08 17:01:47 +0700

    init user service

---
+++ run.bat
@@ -0,0 +1,11 @@
+@echo off
+REM Set JAVA_HOME to JDK 21
+set JAVA_HOME=C:\Program Files\Java\jdk-21
+set PATH=%JAVA_HOME%\bin;%PATH%
+
+REM Optional: Hiển thị version để xác nhận
+echo Using Java version:
+java -version
+
+REM Run Spring Boot project
+mvn spring-boot:run
